9b7878db30924ddfff4259a00111346d8e891360
Muhammad Maroof <muhammad.maroof@aggreko.com>
2023-05-25 23:23:40 +0200

    Recruitment web starter ms service created

---
+++ src/main/java/com/recruitment/web/main/recruitmentwebmain/RecruitmentWebMainApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.web.main.recruitmentwebmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentWebMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentWebMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitment/web/main/recruitmentwebmain/RecruitmentWebMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.web.main.recruitmentwebmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentWebMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
